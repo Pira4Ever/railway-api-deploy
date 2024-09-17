805bb83c7f03aaffceaa75ddab7ee71acc33f7eb
Octavio Piratininga <59701790+Pira4Ever@users.noreply.github.com>
2024-09-17 19:05:22 -0300

    feat: initial commit

---
+++ src/test/java/edu/octavio/railway_api_deploy/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.octavio.railway_api_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
