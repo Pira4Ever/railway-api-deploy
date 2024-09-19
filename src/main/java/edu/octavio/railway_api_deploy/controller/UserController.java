48eb86c2ffe7e432b0a1a8f1484e829ffcbd1b3b
Octavio Piratininga <59701790+Pira4Ever@users.noreply.github.com>
2024-09-19 16:20:00 -0300

    feat: implemented UserController and added OpenAPI dependency

---
+++ src/main/java/edu/octavio/railway_api_deploy/controller/UserController.java
@@ -0,0 +1,33 @@
+package edu.octavio.railway_api_deploy.controller;
+
+import edu.octavio.railway_api_deploy.domain.model.User;
+import edu.octavio.railway_api_deploy.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
